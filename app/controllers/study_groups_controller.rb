cbe14472e778e1a1b9dcff72a735fe65a732f110
KenZheng96 <124521720+KenZheng96@users.noreply.github.com>
2026-04-27 20:42:02 -0500

    Fix merge conflicts, update schema

--- app/controllers/study_groups_controller.rb
+++ app/controllers/study_groups_controller.rb
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-class StudyGroupsController < ApplicationController
-  def index
-    @study_group = StudyGroup.new
-    @users = User.order(:name)
-    @study_groups = StudyGroup.includes(:creator, :members).order(study_time: :asc)
-  end
-
-  def create
-    @study_group = StudyGroup.new(study_group_params)
-    @study_group.tags = parsed_tags
-
-    if @study_group.save
-      @study_group.group_memberships.find_or_create_by!(user_id: @study_group.creator_id)
-      redirect_to study_groups_path, notice: "Study group was successfully created."
-    else
-      @users = User.order(:name)
-      @study_groups = StudyGroup.includes(:creator, :members).order(study_time: :asc)
-      render :index, status: :unprocessable_entity
-    end
-  end
-
-  def join
-    study_group = StudyGroup.find(params.expect(:id))
-    user = User.find(params.expect(:user_id))
-
-    membership = study_group.group_memberships.find_or_initialize_by(user: user)
-    if membership.persisted? || membership.save
-      redirect_to study_groups_path, notice: "#{user.name} joined #{study_group.name}."
-    else
-      redirect_to study_groups_path, alert: membership.errors.full_messages.to_sentence
-    end
-  end
-
-  private
-
-  def study_group_params
-    params.expect(study_group: [ :name, :study_time, :location_mode, :communication_style, :creator_id, tags: [] ])
-  end
-
-  def parsed_tags
-    base_tags = study_group_params[:tags].to_a
-    custom_tags = params[:extra_tags].to_s.split(",").map(&:strip)
-    (base_tags + custom_tags).reject(&:blank?).uniq
-  end
-=======
 class StudyGroupsController < ApplicationController
   before_action :require_login
   before_action :set_study_group, only: [ :show, :join ]
@@ -96,7 +50,6 @@ class StudyGroupsController < ApplicationController
 
   def ensure_group_member!
     return if @study_group.members.exists?(id: current_user.id)
-
     redirect_to study_groups_path, alert: "You need to join this group before you can enter it."
   end
 
@@ -109,5 +62,4 @@ class StudyGroupsController < ApplicationController
     custom_tags = params[:extra_tags].to_s.split(",").map(&:strip)
     (base_tags + custom_tags).reject(&:blank?).uniq
   end
->>>>>>> f83138d5235a9ded27a54464777884f537da4598
 end
