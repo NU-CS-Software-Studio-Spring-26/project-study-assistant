8492c68a29752f005e0541035883a7e4d04c2a65
Andrew Elias <andelias03@gmail.com>
2026-05-13 01:20:41 -0500

    added description migration to schema

---
+++ db/migrate/20260513000000_add_description_to_study_groups.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToStudyGroups < ActiveRecord::Migration[8.1]
+  def change
+    add_column :study_groups, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_11_033313) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_13_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_11_033313) do
     t.string "communication_style", null: false
     t.datetime "created_at", null: false
     t.bigint "creator_id", null: false
+    t.text "description"
     t.datetime "end_time", null: false
     t.string "join_password"
     t.string "location_mode", null: false
