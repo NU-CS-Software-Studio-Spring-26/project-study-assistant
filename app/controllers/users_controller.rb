e4df8fae4e3f4b150859c06627bbf5e04ac9caed
KenZheng96 <124521720+KenZheng96@users.noreply.github.com>
2026-05-17 17:09:02 -0500

    add landing page, ical tooltip, auto-sync on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,23 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy sync_ical ]
   before_action :ensure_current_user, only: %i[ show edit update destroy sync_ical ]
 
-  # GET /users/1 or /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
+        IcalSyncService.new(@user).sync if @user.ical_url.present?
         format.html { redirect_to dashboard_path, notice: "Account created. Welcome to Study Assistant." }
         format.json { render :show, status: :created, location: @user }
       else
@@ -32,7 +29,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -45,7 +41,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy!
 
@@ -66,12 +61,11 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       permitted = params.expect(user: [ :name, :email, :ical_url, :password, :password_confirmation ])
       if permitted[:password].blank? && permitted[:password_confirmation].blank?
